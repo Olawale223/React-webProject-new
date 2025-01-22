a06e4e91f024cfed87e02db2af0a78884c8bccb8
Iwuchukwu Chidera Paul <chideraiwuchukwu@gmail.com>
2025-01-22 02:47:28 +0100

    another night

--- project-app/src/Components/Minor Components/SlotComponent.jsx
+++ project-app/src/Components/Minor Components/SlotComponent.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './SlotComponent.css';
+import roleContent from '../Roles'
 
-export default function SlotComponent() {
+export default function SlotComponent({image, name, role}) {
   return (
-    <div>
-      
+    <div className="team-section">
+      <div className='member-image-container'>
+      <img src={image} alt={name} className='member-image'/>
+      </div>
+      <div className="team-member">
+        <h3>{name}</h3>
+        <p>{role}</p>
+      </div>
     </div>
   )
 }
