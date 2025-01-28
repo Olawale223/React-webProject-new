fa21e7a8d0156f92cddc9fd49d08d0caf511b84e
Iwuchukwu Chidera Paul <chideraiwuchukwu@gmail.com>
2025-01-28 01:16:33 +0100

    floating image

--- project-app/src/Components/Minor Components/Floatingimage.jsx
+++ project-app/src/Components/Minor Components/Floatingimage.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import './CardComponent.css'
+import phonee from '../../Assets/FoodCourt/phonee.png';
+import play from '../../Assets/FoodCourt/play.svg';
+import apple from '../../Assets/FoodCourt/apple.svg';
 
-
-export default function Floatingimage() {
+export default function FloatingImage() {
   return (
-    <div className='floating-cont'>
+    <div className="floating-cont">
       <div className="fl-text-cont">
-        <h1>Sunny Side Club</h1>
-        <p>Good food to start your day right!</p>
+        <h1>Hungry?</h1>
+        <p>Order now!</p>
         <div className="fl-img">
-            <img src="" alt="" />
-            <img src="" alt="" />
+          <a href="#" className="store-btn">
+            <img src={apple} alt="Download on the App Store" className="app-store" />
+            <div>Download on the App Store</div>
+          </a>
+          <a href="#" className="store-btn">
+            <img src={play} alt="Get it on Google Play" className="play-store" />
+            <div>Get it on Google Play</div>
+          </a>
         </div>
       </div>
       <div className="fl-img-cont">
-        <img src="" alt="" />
+        <img
+          src={phonee}
+          alt="Mobile app interface preview"
+          // className="phone-mockup"
+        />
       </div>
-
     </div>
   )
 }
+
